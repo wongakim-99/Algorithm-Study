8fe5b3cf1e82e1c1d1a61690d4b880cf3063e293
gawonkim <kgw1999zz@naver.com>
2024-02-15 22:06:45 +0900

    feat: 백준_25304_영수증

--- study/src/gawonkim/Baekjoon/boj_25304.py
+++ study/src/gawonkim/Baekjoon/boj_25304.py
@@ -1,7 +1,7 @@
 receipt_total_price = int(input())  #영수증에 적힌 전체 가격
 product_type = int(input())   #물건종류
 
-temp_sum = 0    #내가 계산해본 전체 가격
+temp_sum = 0   #내가 계산해본 전체 가격
 for i in range(product_type):
     product_price, product_count = map(int,input().split()) #각 물건가격, 각 물건개수
     temp_price = product_price*product_count    #내가 계산한 각 물건가격x각 물건개수
@@ -11,3 +11,5 @@ if temp_sum == receipt_total_price:
     print("Yes")
 else:
     print("No")
+
+'''10950,8393,25304,2739'''
