88fdb179b401c7175c5607aa7f64df086f3a588f
gawonkim <kgw1999zz@naver.com>
2024-02-18 21:45:54 +0900

    feat : 백준_10810_공넣기

---
+++ study/src/gawonkim/Baekjoon/boj_10810.py
@@ -0,0 +1,12 @@
+#백준 10810 공 넣기
+
+N, M = map(int, input().split())    #N = 바구니 개수, M = 공을 넣을 횟수
+bucket = [0]*(N+1)
+
+for a in range(M):
+    i,j,k = map(int,input().split())
+    for n in range(i, j+1):
+        bucket[n] = k
+
+for i in range(1, N+1):
+    print(bucket[i], end = ' ')
