a620f8bb0a6b70ec2137345be9c9b4417bcaffa6
gawonkim <kgw1999zz@naver.com>
2024-02-20 16:27:56 +0900

    feat : 백준_11720_숫자의 합

---
+++ study/src/gawonkim/Baekjoon/boj_11720.py
@@ -0,0 +1,15 @@
+# n = int(input())
+# nums = input()
+
+# total = 0
+# for i in range(n):
+#     total += int(nums[i])
+
+# print(total)
+
+
+# 또 다른 버전 - 라이브러리 활용
+# num = input()
+# numbers = list(map(int,input()))
+
+# print(sum(numbers))
