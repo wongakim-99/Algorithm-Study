10672810e611d6849a3aeb48a4376c12719bfa58
wongakim-99 <202021000@sangmyung.kr>
2024-05-04 17:19:17 +0900

    feat : boj_18258_큐2

---
+++ study/src/gawonkim/Baekjoon/boj_18258.py
@@ -0,0 +1,39 @@
+import sys
+from collections import deque
+
+queue = deque()
+N = int(sys.stdin.readline())
+
+for _ in range(N):
+    cmd = sys.stdin.readline().split()
+
+    if cmd[0] == 'push':
+        queue.append(int(cmd[1]))
+    
+    elif cmd[0] == 'pop':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue[0])
+            queue.popleft()
+    
+    elif cmd[0] == 'size':
+        print(len(queue))
+    
+    elif cmd[0] == 'empty':
+        if len(queue) == 0:
+            print(1)
+        else:
+            print(0)
+    
+    elif cmd[0] == 'front':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue[0])
+    
+    elif cmd[0] == 'back':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue[-1])
